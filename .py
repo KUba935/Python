81f039e0ba883289d3eff70ec0ae18fa7d00a40c
KUba935 <kubsonix198@gmail.com>
2025-10-22 15:43:01 +0200

    Add files via upload

---
+++ .py
@@ -0,0 +1,15 @@
+#x = 2
+
+#if x % 2 == 0:
+#    print('Liczba jest parzysta')
+#else:
+#    print('Liczba nie jest parzysta')
+
+#x = int(input('Podaj wiek: '))
+
+#if x < 18:
+
+#    print('Jesteś nastolatkiem')
+#else:
+#    print('Jesteś dorosłym')
+
