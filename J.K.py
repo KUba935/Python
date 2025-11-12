587bbf5c3f3350aa38d92b482d6ee85619aa1d40
KUba935 <kubsonix198@gmail.com>
2025-11-12 15:40:24 +0100

    Add files via upload

---
+++ J.K.py
@@ -0,0 +1,24 @@
+#x = 4
+
+
+#if x % 2 == 0:
+#    print('liczba parzysta')
+#else:
+#    print('liczba nieparzysta')
+
+
+
+
+#print('na co chcesz zmienić Celciusz (C) czy Farenhait')
+# rodzaj = input(('podaj temperature: ').lower)
+
+#if rodzaj == 'F':
+#    celcius = int(input('podaj temperature: '))
+#    dzialaniefaren = (celcius * 1.8) + 32
+#    print(dzialaniefaren)
+#elif rodzaj == 'C':
+#   faren = int(input('podaj temperature: '))
+#    dzialaniecelcius = (faren - 32) * 5/9 
+#   print(dzialaniecelcius)
+#else:
+#    print('podaj inną litere')
