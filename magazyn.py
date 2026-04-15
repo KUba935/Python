3e307b0dacc954da2a4d6c269645ede168c7ba25
KUba935 <kubsonix198@gmail.com>
2026-04-15 14:43:44 +0200

    Add files via upload

--- magazyn.py
+++ magazyn.py
@@ -19,28 +19,28 @@ while(True):
     print("3) Sprawdź dostępność produktu")
     print("4) Wyświetl wszystkie produkty")
     print("X) Wyjdź")
-    Wesker = int(input("Opcja nr : "))
+    Wesker = input("Opcja nr : ")
     if Wesker == '1':
-        slownik = input("Do której kategorii dodać: ")
-        key = input("Wprowadź co dodać jako produkt: ")
-        value = int(input("Wprowadź ile produktów dodać: "))
-        lidl[slownik][key] = value
+        kot = input("Do czego dodać: ")
+        key = input("Co chcesz jako produkt: ")
+        value = int(input("Podaj ilość produktów: "))
+        lidl[kot][key] = value
         print(lidl)
         break
     elif Wesker == '2':
-        slownik2 = input("Z jakiej kategorii usunąć: ")
-        key2 = input("Wprowadź co usunąć: ")
-        del lidl[slownik2][key2]
+        kot2 = input("Z czego chcesz usunąć: ")
+        key2 = input("Co chcesz usunąć: ")
+        del lidl[kot2][key2]
         print(lidl)
         break
     elif Wesker == '3':
-        slownik3 = input("Jaką kategorie chcesz zobaczyć?: ")
+        kot3 = input("Czego dostępność chcesz zobaczyć: ")
         key3 = input("Wprowadź co wyświetlić: ")
-        print(lidl[slownik3][key3])
+        print(lidl[kot3][key3])
         break
     elif Wesker == '4':
-        slownik4 = input("Jaką kategorie chcesz zobaczyć?: ")
-        lidl[slownik4]
+        kot4 = input("Jaką kategorie chcesz zobaczyć?: ")
+        lidl[kot4]
         print(lidl)
         break
     elif Wesker == "X":
