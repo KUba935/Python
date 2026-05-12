a83773fcec2c544b6d031af96452ceeccf4b5841
KUba935 <kubsonix198@gmail.com>
2026-05-12 12:34:26 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,24 @@
+def funkcja():
+    rok = int(input("Podaj rok"))
+    if ()
+
+
+
+
+
+rzymskie = {
+    "I" : 1,
+    "Iv" : 4,
+    "VI" : 6,
+    "VII" : 7,
+    "X" : 10,
+    "XXX" : 30,
+    "L" : 50,
+    "LXX" : 70,
+    "C" : 100,
+    "D" : 500,
+    "M" : 1000,
+}
+
+
+
